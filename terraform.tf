303c1ee38b717e9ace3aac1dd434c52579348f49
luniemma <113865535+luniemma@users.noreply.github.com>
2023-10-29 23:20:45 -0500

    first commit eks-deployent

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.46.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.46.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+~
+
